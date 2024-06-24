10d64be48dd619b27289bb514c062f3fc0436899
Yash Patel <yashbpatel2004@gmail.com>
2024-06-24 15:51:51 -0400

    added typeWriter effect

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,17 +2,17 @@ import React from 'react'
 import '../App.css';
 import './Section.css';
 import video from './video.mp4';
-import { useEffect, useRef } from 'react';
-
+import {useState, useEffect, useRef } from 'react';
+import TypeWriterEffect from 'react-typewriter-effect';
 function Section() {
   const videoRef = useRef(null);
   const textRef = useRef(null);
-  
+
   const handleTimeUpdate = () => {
     const currentTime = videoRef.current.currentTime;
     const duration = videoRef.current.duration;
     
-    if (duration - currentTime <= 49) {
+    if (duration - currentTime <= 30) {
       // Apply blur effect (add a CSS class to the video element)
       videoRef.current.classList.add('blur-effect');
       textRef.current.classList.remove('invisible-text');
@@ -23,10 +23,24 @@ function Section() {
       textRef.current.classList.add('invisible-text');
     }
   };
+  
 
   return(
     <div> 
-      <p className='invisible-text' ref={textRef}>Best In Class Estate Management Solutions</p>
+      <p className='invisible-text' ref={textRef}> <a>Best In Class</a> <TypeWriterEffect 
+        textStyle={{}}
+        startDelay={2000}
+        cursorColor="#fff"
+        multiText={[
+          '  Business Management Solutions.',
+          ' Gas Stations.',
+          ' Restauraunts',
+          ' Estate Management Solutions.',
+        ]}
+        multiTextDelay={1000}
+        typeSpeed={30}
+        loop = {true}
+      /></p>
       <video ref={videoRef} src={video} autoPlay muted onTimeUpdate={handleTimeUpdate}/>
     </div>
   )
